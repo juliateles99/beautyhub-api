df70736e2c4fcd18a2106363e24271dc40f62d78
juliateles99 <juliateles.github@gmail.com>
2025-05-21 12:34:46 -0300

    chore: adiciona configuração inicial do pom.xml com Spring Boot, JPA, Lombok, PostgreSQL e Swagger

---
+++ src/main/java/com/julia/beautyhub/BeautyhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.julia.beautyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyhubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyhubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julia/beautyhub/BeautyhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.beautyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyhubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
